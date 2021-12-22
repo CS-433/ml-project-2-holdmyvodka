5c75dcf1a8a9733ad17bcb209eafc4544f4435a0
Chen Zhao <sailorzzcc@gmail.com>
2021-12-22 21:43:19 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-python registration.py --img1_path /data/ml4s2021/data/2021_11_12/heyex/CG_00000034-20191016-R-multicolor-heyex-00002.jpg --img2_path /data/ml4s2021/data/2021_11_12/heyex/CG_00000034-20191217-R-other-heyex-00001.jpg --method SuperGlue 
-
+python registration.py --img1_path ./demo/img1.png --img2_path ./demo/img2.png --method SuperGlue 
